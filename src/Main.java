92c7a7761af3fef798ccf21aacb57fb1dfcc6c54
artemudotest1 <artemudotest1@gmail.com>
2020-08-13 13:26:57 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Artem");
         System.out.println("Привет, Артем");
+        System.out.println("Привет Мир");
 
     }
 }
